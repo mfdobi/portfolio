fe09de84c151b7aeb943f4e408b3b0beb9c09e8b
marcdobner <mfdobner@googlemail.com>
2022-07-22 10:27:29 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'utilities.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Color.fromRGBO(238, 235, 224, 1),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -73,52 +75,143 @@ class _MyHomePageState extends State<MyHomePage> {
            ),
            SizedBox(height: 50,),
            Center(child: Text("SKILLS",style: GoogleFonts.cutive(textStyle: TextStyle(fontSize: 32)),)),
-           Container(
-             height: 200,
-             color: Colors.grey,
-             padding: EdgeInsets.only(top: 25),
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-               children: [ 
-                 Column(
-                   children: [
-                     Text("WEB(FRONTEND)"),
-                     Text("-HTML5"),
-                     Text("-CSS(basics)"),
-                     Text("-Flutter for Web")],),
 
-                 Column(
-                   children: [
-                     Text("MOBILE"),
-                     Text("-Flutter for ios&android")],),
+           FittedBox(
+             fit: BoxFit.scaleDown,
+             child: Container(
+               height: 200,
+               //color: Colors.grey,
+               padding: EdgeInsets.only(top: 25,left: 5,right: 5),
+               margin: EdgeInsets.only(left: 25,right: 25),
+               decoration: BoxDecoration(
+                   border: Border(
+                       bottom: BorderSide(width: 3,color: Colors.black26),
+                       top: BorderSide(width: 3,color: Colors.black26),
+                   )),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                 children: [
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.only(bottom: 8),
+                         child: Text("WEB(FRONTEND)",style: table_text_styling1,),
+                       ),
+                       Text("-HTML5",style: table_text_styling,),
+                       Text("-CSS(basics)",style: table_text_styling,),
+                       Text("-Flutter for Web",style: table_text_styling,)],),
 
-                 Column(
-                   children: [
-                     Text("WEB(BACKEND)"),
-                     Text("-Flask"),
-                     Text("-Django")],),
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.only(bottom: 8),
+                         child: Text("MOBILE",style: table_text_styling1,),
+                       ),
+                       Text("-Flutter for ios&android",style: table_text_styling,)],),
 
-                 Column(
-                   children: [
-                     Text("OTHER FRAMEWORKS/TECHNIQUES"),
-                     Text("-Pytorch"),
-                     Text("-Python for scripting/General"),
-                     Text("-Dart for scripting"),
-                     Text("-Database Management with ORMS like Django orm and peewee")],),
-               ],
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.only(bottom: 8),
+                         child: Text("WEB(BACKEND)",style: table_text_styling1,),
+                       ),
+                       Text("-Flask",style: table_text_styling,),
+                       Text("-Django",style: table_text_styling,)],),
+
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.only(bottom: 8),
+                         child: Text("OTHER FRAMEWORKS/TECHNIQUES",style: table_text_styling1,),
+                       ),
+                       Text("-Pytorch",style: table_text_styling,),
+                       Text("-Python for scripting/General",style: table_text_styling,),
+                       Text("-Dart for scripting",style: table_text_styling,),
+                       Text("-Database Management with ORMS like Django orm and peewee",style: table_text_styling,)],),
+                 ],
+               ),
              ),
            ),
+
+
            Padding(
-             padding: const EdgeInsets.only(top: 25),
+             padding: const EdgeInsets.only(top: 25,bottom: 20),
              child: Text("MY PROJECTS",style: GoogleFonts.cutive(textStyle: TextStyle(fontSize: 32)),),
            ),
-           Text("Here are some of my Projects I've build.\n"
-               "Note: Some projects are older and might not be (fully) online. "
-               "But I can provide some more Information or a Demo if you want.", style: GoogleFonts.cutive(),),
-           Container(height: 200,color: Colors.black12,),
-           Container(height: 200,color: Colors.black12,),
-           Container(height: 200,color: Colors.black12,),
-           Container(height: 200,color: Colors.black12,),
+           Padding(
+             padding: const EdgeInsets.only(bottom: 20),
+             child: Text("Here are some of my Projects I've build.\n"
+                 "Note: Some projects are older and might not be (fully) online. "
+                 "But I can provide some more Information or a Demo if you want.", style: GoogleFonts.cutive(),),
+           ),
+
+           
+           Container(
+             height: 200,
+             decoration: BoxDecoration(
+               //color: Colors.black26,
+               border: Border(top: BorderSide(width: 3,color: Colors.black26),bottom: BorderSide(width: 3,color: Colors.black26))
+             ),
+             child: Row(
+               children: [
+                 Column(
+                   children: [
+                     Text("Ticketing system"),
+                     Text("Description of ticketing system"),
+                     Text("link to more details")
+                   ],
+                 ),
+                 Container(width: 100,height: 100,color: Colors.black26)
+               ],
+             ),),
+           Container(
+             height: 200,
+             color: Colors.black12,
+             child: Row(
+               children: [
+                 Container(
+                   width: 200,
+                   height: 200,
+                   color: Colors.black26,
+                 ),
+                 Column(
+                   children: [
+                     Text("Abode"),
+                     Text("Browse Property listings by simply swiping through them!!"),
+                     Text("links to app store"),
+                     Text("hint: servers may have some downtime")
+                   ],
+                 ),
+               ],
+             ) ,),
+           Container(
+             height: 200,
+             color: Colors.black12,
+             child: Row(
+               children: [
+                 Column(
+                   children: [
+                     Text("Ai Art"),
+                     Text("Description"),
+                   ],
+                 ),
+                 Container(width: 200,height: 200,color: Colors.black26,)
+               ],
+             ) ,),
+           Container(
+             height: 200,
+             color: Colors.black12,
+             child: Row(
+               children: [
+                 Container(width: 200,height: 200,color: Colors.black26,),
+                 Column(
+                   children: [
+                     Text("Substitute table viewer"),
+                     Text("Description short"),
+                     Text("link to tech details"),
+                   ],)
+               ],
+             ),),
            Text("Get in Touch")
          ],
        ),
@@ -183,7 +276,14 @@ class terminal extends StatelessWidget {
             height:225 ,
             width: MediaQuery.of(context).size.width*0.5,
             color: Colors.black45,
-            child: Text("That means I do Frontend with Flutter and Backend with Python\n I'm also open to learn new languages and other Stacks if its better for the Project.",style: GoogleFonts.cutive(textStyle: TextStyle(color: Colors.white)),),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5,left: 2,right: 2),
+              child: Text("That means I do Frontend with Flutter and Backend with Python\n "
+                  "I'm also open to learn new languages and other Stacks if its better for the Project."
+                  "\n\n\nMy focus is mainly on programming, but I still managed to add some gimicks to this website."
+                  "(hint: Buttons!!)\n\nFeel free to try them out.",
+                style: GoogleFonts.cutive(textStyle: TextStyle(color: Colors.white)),),
+            ),
           )
         ],
       ),
---
+++ lib/utilities.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+var table_text_styling1 =  GoogleFonts.cutive(textStyle: TextStyle(fontSize: 25,color: Colors.black));
+var table_text_styling =  GoogleFonts.cutive(textStyle: TextStyle(fontSize: 20,color: Colors.black));
+
+//video link for ticketing system
+// https://youtube.com/shorts/UhoASUbdhgc
